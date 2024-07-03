d5a0defcc5fa77ad664581e134b0d324031c7044
Vais88 <xvaisx@bk.ru>
2024-07-03 14:02:37 +0300

    Init MyService

---
+++ app/src/main/java/com/vais/servicestest/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.vais.servicestest
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.vais.servicestest.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        binding.simpleService.setOnClickListener {
+            startService(MyService.newIntent(this))
+        }
+    }
+}
