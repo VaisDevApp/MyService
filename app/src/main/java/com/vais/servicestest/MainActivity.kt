d6742b22996c9cac47f8f82932f058b16c52f0bf
Vais88 <xvaisx@bk.ru>
2024-07-03 16:18:04 +0300

    creating_foreground_service

--- app/src/main/java/com/vais/servicestest/MainActivity.kt
+++ app/src/main/java/com/vais/servicestest/MainActivity.kt
@@ -1,21 +1,27 @@
 package com.vais.servicestest
 
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.vais.servicestest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         binding.simpleService.setOnClickListener {
-            startService(MyService.newIntent(this))
+            startService(MyService.newIntent(this, 25))
+        }
+        binding.foregroundService.setOnClickListener {
+            ContextCompat.startForegroundService(this, MyForegroundService.newIntent(this))
         }
     }
+
 }
